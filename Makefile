eca023bb8bb95b71db5f7f9bde774babc2febccc
Gianluca Arbezzano <gianarb92@gmail.com>
2018-05-21 21:36:43 +0200

    Bootstrapped RestAPI
    This commit boostrap the api system.
    Currently it supports a single request:
    
    ```
    curl -X GET -v localhost:8080/api/trace/3453
    {
      "spans": [
        {
          "trace": "1234",
          "id": "1",
          "name": "nginx",
          "start_time": "2011-08-30T09:30:16.768-04:00",
          "duration_ns": 58000000000
        },
        {
          "trace": "1234",
          "id": "2",
          "name": "gateway",
          "start_time": "2011-08-30T09:31:16.768-04:00",
          "duration_ns": 10000000000
        },
        {
          "trace": "1234",
          "id": "3",
          "name": "kafka",
          "start_time": "2011-08-30T09:31:50.768-04:00",
          "duration_ns": 2000000000
        },
        {
          "trace": "1234",
          "id": "4",
          "name": "etcd",
          "start_time": "2011-08-30T09:32:00.768-04:00",
          "duration_ns": 5563463456
        }
      ],
      "rels": [
        {
          "trace": "1234",
          "parent_id": "1",
          "id": "2"
        },
        {
          "trace": "1234",
          "parent_id": "2",
          "id": "3"
        },
        {
          "trace": "1234",
          "parent_id": "1",
          "id": "4"
        }
      ]
    }
    ```
    
    Signed-off-by: Gianluca Arbezzano <gianarb92@gmail.com>

---
+++ Makefile
@@ -0,0 +1,2 @@
+sherlockd/build:
+	go build -o ./bin/sherlockd backend/cmd/sherlockd/main.go
---
+++ backend/cmd/sherlockd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"sync"
+
+	"github.com/garethr/kubeval/log"
+	"github.com/gianarb/sherlock/backend/restapi"
+)
+
+func main() {
+	log.Info("Sherlock version ciao")
+	noUi := flag.Bool("no-ui", false, "a bool")
+	flag.Parse()
+	restConf := &restapi.Config{
+		NoUi: noUi,
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(rconf *restapi.Config) {
+		closeConn := restapi.StartRestApi(8080, rconf)
+		defer closeConn()
+	}(restConf)
+	wg.Wait()
+}
